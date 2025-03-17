a84ffe5fafc552d6b30c9316d5bda987831cce3a
19951680900 <2604211051@qq.com>
2025-03-17 10:22:30 +0800

    claim

--- src/MerkleAirdrop.sol
+++ src/MerkleAirdrop.sol
@@ -8,10 +8,17 @@ contract MerkleAirdrop {
     using SafeERC20 for IERC20;
 
     error MerkleAirdrop__InvalidProof();
+    error MerkleAirdrop__AlreadyClaimed();
 
-    address[] claimers;
-    bytes32 private immutable i_merkleRoot;
+    /**
+     * State Variables
+     */
+    address[] claimers; // 领取者的地址
+    bytes32 private immutable i_merkleRoot; // 根节点hash值
     IERC20 private immutable i_airdropToken;
+    mapping(address claimer => bool claimed) private s_hasClaimed; // 是否已经领取
+
+    event Claim(address account, uint256 amount);
 
     constructor(bytes32 merkleRoot, IERC20 airdropToken) {
         i_merkleRoot = merkleRoot;
@@ -23,14 +30,22 @@ contract MerkleAirdrop {
         uint256 amount,
         bytes32[] calldata merkleProof
     ) external {
-        // 生成哈希值
+        // 检查是否已经领取
+        if (s_hasClaimed[account]) {
+            revert MerkleAirdrop__AlreadyClaimed();
+        }
+        // 生成哈希值，2次操作防止哈希碰撞
         bytes32 leaf = keccak256(
             bytes.concat(keccak256(abi.encode(account, amount)))
         );
+        // 与根节点哈希值比较
         if (!MerkleProof.verify(merkleProof, i_merkleRoot, leaf)) {
             revert MerkleAirdrop__InvalidProof();
         }
-        // emit Claim(account, amount);
+        // 标记已经领取
+        s_hasClaimed[account] = true;
+        // 发送代币
+        emit Claim(account, amount);
         i_airdropToken.safeTransfer(account, amount);
     }
 }
